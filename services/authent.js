c4eac07b50b12f6d23179d152eb5b2f2c9f33f8f
Xavier Nouaille <xavier.nouaille@protonmail.com>
2020-10-12 21:11:23 +0200

    Add bcrypt for signup and regex password

--- services/authent.js
+++ services/authent.js
@@ -1,6 +1,7 @@
 require('mongoose')
 const User = require('../models/user')
 const db = require('../config/database')
+const bCrypt = require('bcrypt')
 
 exports.signin = async (username, password) => {
     try {
@@ -17,16 +18,28 @@ exports.signin = async (username, password) => {
 }
 
 exports.signup = async (name, lastname, username, password, confirm) => {
+    const passwordRegexp = RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$')
     try {
         if (name != '' && lastname != '' && username != '' && password != '' && confirm != '') {
-            if (password != confirm) {
+            if (!passwordRegexp.test(password)) {
+                throw 'Password need to have at lease one digits, one lowercase character, one uppercase character, one special character and 8 characters in length'
+            }
+            else if (password != confirm) {
                 throw 'Password is not equal to confirm'
-            } else {
+            }
+            else {
                 const user = await User.findOne({ username: { $regex: username, $options: 'i' } })
                 if (user) {
-                    throw 'This username is already use' //bug
+                    throw 'This username is already use'
                 } else {
-                    //save user
+                    password = await bCrypt.hash(password, 10)
+                    const newUser = new User({
+                        name: name,
+                        lastname: lastname,
+                        username: username,
+                        password: password
+                    }).save()
+                    return newUser
                 }
             }
         } else {
